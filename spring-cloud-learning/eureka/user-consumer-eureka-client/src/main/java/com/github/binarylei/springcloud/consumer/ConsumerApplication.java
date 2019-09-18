4956cbeed6bac85743bc5c9e3a8ed3d933cfe1d6
binarylei <binarylei@qq.com>
2019-09-19 07:29:05 +0800

    feign

--- spring-cloud-learning/eureka/user-consumer-eureka-client/src/main/java/com/github/binarylei/springcloud/consumer/ConsumerApplication.java
+++ spring-cloud-learning/eureka/user-consumer-eureka-client/src/main/java/com/github/binarylei/springcloud/consumer/ConsumerApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author leigang
  * @version 2019-03-19
@@ -13,6 +17,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class ConsumerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConsumerApplication.class);
+//        SpringApplication.run(ConsumerApplication.class);
+
+        List<String> list = Arrays.asList("1.3.2.1", "1.8.2.1", "1.5.2.1", "1.7.2.1");
+        Collections.sort(list);
+
+        System.out.println(list);
+        System.out.println(list.indexOf("1.5.2.1"));
+        System.out.println(list.lastIndexOf("1.5.2.1"));
     }
 }
--- spring-cloud-learning/hystrix/user-consumer-hystrix/src/main/java/com/github/binarylei/springcloud/hystrix/controller/HystrixController.java
+++ spring-cloud-learning/hystrix/user-consumer-hystrix/src/main/java/com/github/binarylei/springcloud/hystrix/controller/HystrixController.java
@@ -2,7 +2,10 @@ package com.github.binarylei.springcloud.hystrix.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
@@ -13,19 +16,21 @@ import java.util.Random;
  */
 @RestController
 public class HystrixController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("hystrix")
     @HystrixCommand(
             commandProperties = {@HystrixProperty(
                     name = "execution.isolation.thread.timeoutInMilliseconds", value = "100")},
-            fallbackMethod = "error")
-    public String hystrix() throws InterruptedException {
-        int sleepTime = new Random().nextInt(200);
-        Thread.sleep(sleepTime);
-        return "sleep: " + sleepTime + " ms";
+            fallbackMethod = "timeout")
+    public String hystrix(@RequestParam int costTime) throws InterruptedException {
+        Thread.sleep(costTime);
+        logger.info("hystrix info");
+        return "sleep: " + costTime + " ms";
     }
 
-    public String error() {
+    public String timeout(@RequestParam int costTime) {
+        logger.error("hystrix error");
         return "timeout";
     }
 }
